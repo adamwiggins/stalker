45be0c5b5e6deb9a9d60af1c9d65adc567e9bd45
Han Kessels <han.kessels@gmail.com>
2011-01-25 16:33:58 +0900

    support for multiple beanstalk servers

--- lib/stalker.rb
+++ lib/stalker.rb
@@ -139,9 +139,9 @@ module Stalker
     STDERR.puts msg
   end
 
-  def beanstalk
-    @@beanstalk ||= Beanstalk::Pool.new([ beanstalk_host_and_port ])
-  end
+	def beanstalk
+		@@beanstalk ||= Beanstalk::Pool.new(beanstalk_addresses)
+	end
 
   def beanstalk_url
     return @@url if defined?(@@url) and @@url
@@ -150,10 +150,15 @@ module Stalker
 
   class BadURL < RuntimeError; end
 
-  def beanstalk_host_and_port
-    uri = URI.parse(beanstalk_url)
-    raise(BadURL, beanstalk_url) if uri.scheme != 'beanstalk'
-    return "#{uri.host}:#{uri.port || 11300}"
+  def beanstalk_addresses
+    uris = beanstalk_url.split(/[\s,]+/)
+    uris.map {|uri| beanstalk_host_and_port(uri)}
+  end
+
+  def beanstalk_host_and_port(uri_string)
+    uri = URI.parse(uri_string)
+    raise(BadURL, uri_string) if uri.scheme != 'beanstalk'
+    "#{uri.host}:#{uri.port || 11300}"
   end
 
   def exception_message(e)
