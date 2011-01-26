9aa89459bdfd1ec781ff335628dbe7681ece1511
Han Kessels <han.kessels@gmail.com>
2011-01-26 11:43:24 +0900

    added unit test and readme update

--- test/stalker_test.rb
+++ test/stalker_test.rb
@@ -8,11 +8,11 @@ module Stalker
 end
 
 class StalkerTest < Test::Unit::TestCase
-  setup do
-    Stalker.clear!
-    $result = -1
-    $handled = false
-  end
+	setup do
+		Stalker.clear!
+		$result = -1
+		$handled = false
+	end
 
   def with_an_error_handler
     Stalker.error do |e, job_name, args|
@@ -106,6 +106,19 @@ class StalkerTest < Test::Unit::TestCase
     assert $handled
     assert_equal 'my.job', $job_name
     assert_equal({'foo' => 123}, $job_args)
+  end
+	
+  test "parse BEANSTALK_URL" do
+    ENV['BEANSTALK_URL'] = "beanstalk://localhost:12300"
+    assert_equal Stalker.beanstalk_addresses, ["localhost:12300"]
+    ENV['BEANSTALK_URL'] = "beanstalk://localhost:12300/, beanstalk://localhost:12301/"
+    assert_equal Stalker.beanstalk_addresses, ["localhost:12300","localhost:12301"]
+    ENV['BEANSTALK_URL'] = "beanstalk://localhost:12300   beanstalk://localhost:12301"
+    assert_equal Stalker.beanstalk_addresses, ["localhost:12300","localhost:12301"]
+    ENV['BEANSTALK_URL'] = "beanstalk://localhost:12300, http://localhost:12301"
+    assert_raise Stalker::BadURL do
+      Stalker.beanstalk_addresses 
+    end
   end
 
 end
